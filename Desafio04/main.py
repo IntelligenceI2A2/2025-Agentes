4c55842815fe22f90bfc0bae1668ed8a19f46566
Matheus Salomão <mathesbh@gmail.com>
2025-08-19 20:11:40 -0300

    feat: inclui classe para processar e calcular os valores dos vales

--- Desafio04/main.py
+++ Desafio04/main.py
@@ -8,6 +8,7 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Processador de VR (versão Python puro, modularizado e orientado a objetos)")
     parser.add_argument("--base-dir", required=True, help="Pasta com as planilhas de entrada")
+    parser.add_argument("--out", required=True, help="Caminho do arquivo .xlsx de saída")
     args = parser.parse_args()
 
     carregador = CarregadorDados(args.base_dir)
@@ -16,7 +17,9 @@ def main():
     dados["deslig"] = carregador.limpar_desligados(dados["deslig_raw"])
     dados["ext"] = carregador.limpar_exterior(dados["ext_raw"])
     dados["sxv"] = carregador.limpar_sindicato_valor(dados["sxv"])
-
+    processador = ProcessadorVR(dados)
+    caminho_saida = processador.processar(args.out)
+    print(f"Arquivo gerado em: {caminho_saida}")
 
 if __name__ == "__main__":
     main()

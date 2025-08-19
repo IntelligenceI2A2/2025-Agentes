9b26dc90934f29efa7f5f269706844a36cf07077
Matheus Salomão <mathesbh@gmail.com>
2025-08-19 20:10:18 -0300

    feat: inclui classe e main para carregar dados

---
+++ Desafio04/main.py
@@ -0,0 +1,22 @@
+from processamento import ProcessadorVR
+from carregamento import CarregadorDados
+import argparse
+
+def main():
+    """
+    Função principal que executa o processamento do VR.
+    """
+    parser = argparse.ArgumentParser(description="Processador de VR (versão Python puro, modularizado e orientado a objetos)")
+    parser.add_argument("--base-dir", required=True, help="Pasta com as planilhas de entrada")
+    args = parser.parse_args()
+
+    carregador = CarregadorDados(args.base_dir)
+    dados = carregador.carregar_todas_planilhas()
+    dados["dias"] = carregador.limpar_dias_uteis(dados["dias"])
+    dados["deslig"] = carregador.limpar_desligados(dados["deslig_raw"])
+    dados["ext"] = carregador.limpar_exterior(dados["ext_raw"])
+    dados["sxv"] = carregador.limpar_sindicato_valor(dados["sxv"])
+
+
+if __name__ == "__main__":
+    main()
